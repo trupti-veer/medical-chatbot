198b6d3a474fa0755729aa3608be15e62eb1499d
Trupti Veer <jagtap.trupti@gmail.com>
2024-08-20 15:16:34 +0530

    Adding folder structure.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="medical-chatbot",
+    name="Medical Chatbot using Llama3.1",
     version="0.0.1",
     author="Trupti Veer",
     author_email="jagtap.trupti@gmail.com",
