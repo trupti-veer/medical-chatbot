2c7c245c22f6738c10ecc4799e380a86e3219312
Trupti Veer <jagtap.trupti@gmail.com>
2024-08-20 14:52:15 +0530

    Initial version with notebook implementation

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical-chatbot",
+    version="0.0.1",
+    author="Trupti Veer",
+    author_email="jagtap.trupti@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
